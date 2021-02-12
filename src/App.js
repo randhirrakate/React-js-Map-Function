4429f6df37d922f11e4a4d65186b29dfc8a3ebfd
randhirrakate <randhirrakate2610@gmail.com>
2021-02-12 17:17:07 +0530

    mapfunction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './MyCss.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+
+  const allowedState =
+  [
+    { MyCar: "BMW", Owner: "ABC" },
+    { MyCar: "TESLA", Owner: "ELON" },
+    { MyCar: "TATA", Owner: "RATAN TATA" },
+  ];
+
+
+  return(
+    <div>
+      <h1>Select Tag (Dropdown)</h1>
+
+      <select>
+        {allowedState.map((i, index) => (
+          <option key={i.id}>{i.MyCar}</option>
+        ))}
+      </select>
+
+
+      <h1>ol Tag (Order List)</h1>
+
+      <ol>
+        {allowedState.map((i, index) => (
+          <li key={i.id}>{i.MyCar}</li>
+        ))}
+      </ol>
+
+
+      <h1>ul Tag (Unorder List)</h1>
+
+      <ul>
+        {allowedState.map((i, index) => (
+          <li key={i.id}>{i.MyCar}</li>
+        ))}
+      </ul>
+
+
+      <h1>Table</h1>
+      
+      <table className="MyTable">
+          <tr>
+            <td>Sr No</td>
+            <td>Name</td>
+            <td>Owner Name</td>
+          </tr>
+          {allowedState.map((i, index) => (
+          <tr>
+            <td key={i.id}>{index+1}</td>
+            <td key={i.id}>{i.MyCar}</td>
+            <td key={i.id}>{i.Owner}</td>
+          </tr>
+        ))}
+      </table>
+
     </div>
   );
+  
 }
 
 export default App;
